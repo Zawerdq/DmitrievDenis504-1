116d2caa4f68c37183cea9d97891d598a7fae32a
DmitrievDenis504-1 <dmitriev.denis@gmail.com>
2025-10-17 17:07:35 +0300

    Seminar222

--- Seminar3/Простоесть.py
+++ Seminar3/Простоесть.py
@@ -1,23 +1,3 @@
-n=int(input())
-def func(n,k=2):
-        if n<2:
-                return[]
-        if pow(k,2) > n:
-                return[n]
-        if n%k==0:
-                return([k] + func(n//k,k))
-        else:
-                return func(n,k+1)
-list1=func(n)
-list2=list1
-count=0
-for i in range(1000):
-        if i in list2:
-                k=list2.count(i)
-                while i in list2:
-                    list2.remove(i)
-                s=str(i)+'^'+str(k)+'*'
-                s=list(s)
-                list2.extend(s)
-list2.pop()
-print(''.join(list2))
+a='asld;asld321592'
+b=[int(i) for i in a if '0' <= i <= '9']
+print(b)
