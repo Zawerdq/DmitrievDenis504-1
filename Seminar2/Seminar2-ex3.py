4829e4431f00cab6a18c14fa99f251080f86311d
DmitrievDenis504-1 <dmitriev.denis@gmail.com>
2025-10-17 17:03:34 +0300

    Seminar2

--- Seminar2/Seminar2-ex3.py
+++ Seminar2/Seminar2-ex3.py
@@ -1,3 +1,41 @@
 s=input()
-palindrome=s[::-1]==s
-print(palindrome)
+palendrome=s[::-1]==s
+mirror_dict={
+    'A': 'A',
+    'Y': 'Y',
+    'M': 'M',
+    '1': '1',
+    '8': '8',
+    'H': 'H',
+    'I': 'I',
+    'O': 'O',
+    'T': 'T',
+    'U': 'U',
+    'V': 'V',
+    'W': 'W',
+    'X': 'X',
+    'E': '3',
+    'J': 'L',
+    'S': '2',
+    'Z': '5',
+    '3': 'E',
+    'L': 'J',
+    '2': 'S',
+    '5': 'Z'
+}
+mirror_s=''
+for character in s:
+    if character not in mirror_dict.keys():
+        break
+    mirror_s += mirror_dict[character]
+#map() - думайте
+print(mirror_s)
+mirrored = mirror_s[::-1]==s
+if mirrored and palendrome:
+    print(s+ ' is a mirrored palindrome')
+elif mirrored:
+    print(s+ 'is a mirrored string')
+elif palendrome:
+    print(s+ ' is a palindrome')
+else:
+    print(s+ ' is not a palindrome')
